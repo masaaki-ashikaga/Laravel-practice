4b4e3aa951aa5165855a3dc840914e5fa0b9a196
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-05-19 22:07:06 +0900

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+    //
+    public function index()
+    {
+        $values = Test::all();
+
+        $tests = DB::table('tests')
+        ->select('id')
+        ->get();
+
+        // $collection = collect([1, 2, 3, 4, 5, 6, 7]);
+        // $chunks = $collection->chunk(4);
+        // $chunks->toArray();
+
+        //die + var_dump()
+        dd($tests);
+
+        return view('tests.test', compact('values'));
+    }
+}
