bde62cd2d8cdc7fe6c90bd1ab4778df5f8961bf1
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-05-21 08:28:42 +0900

    update機能作成

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\ContactForm;
+use Illuminate\Support\Facades\DB;
 
 
 class ContactFormController extends Controller
@@ -16,8 +17,18 @@ class ContactFormController extends Controller
      */
     public function index()
     {
-        //
-        return view('contact.index');
+        //Eloquentで全データを取得 ORマッパー
+        // $contacts = ContactForm::all();
+        
+        //クエリビルダ
+        $contacts = DB::table('contact_forms')
+        ->select('id', 'your_name', 'title', 'created_at')
+        ->orderBy('created_at', 'asc')
+        ->get();
+
+        //dd($contacts);
+    
+        return view('contact.index', compact('contacts'));
     }
 
     /**
@@ -64,6 +75,35 @@ class ContactFormController extends Controller
     public function show($id)
     {
         //
+        $contact = ContactForm::find($id);
+        
+        if($contact->gender === 0){
+            $gender = '男性';
+        } else{
+            $gender = '女性';
+        }
+
+        if($contact->age === 1){
+            $age = '〜19歳';
+        }
+        if($contact->age === 2){
+            $age = '20歳〜29歳';
+        }
+        if($contact->age === 3){
+            $age = '30歳〜39歳';
+        }
+        if($contact->age === 4){
+            $age = '40歳〜49歳';
+        }
+        if($contact->age === 5){
+            $age = '50歳〜59歳';
+        }
+        if($contact->age === 6){
+            $age = '60歳〜';
+        }
+
+        return view('contact.show', compact('contact', 'gender', 'age'));
+
     }
 
     /**
@@ -75,6 +115,9 @@ class ContactFormController extends Controller
     public function edit($id)
     {
         //
+        $contact = ContactForm::find($id);
+
+        return view('contact.edit', compact('contact'));
     }
 
     /**
@@ -87,6 +130,20 @@ class ContactFormController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $contact = ContactForm::find($id);
+        //$contact = new ContactForm;
+
+        $contact->your_name = $request->input('your_name');
+        $contact->title = $request->input('title');
+        $contact->email = $request->input('email');
+        $contact->url = $request->input('url');
+        $contact->gender = $request->input('gender');
+        $contact->age = $request->input('age');
+        $contact->contact = $request->input('contact');
+
+        $contact->save();
+        return redirect('contact/index');
+
     }
 
     /**
